d76090c81a95406d90b17b44a6367f6464c7bc0f
Jed-morris <alegre.jed@gmail.com>
2024-03-14 02:09:24 +0800

    update db_conn

--- db_conn.php
+++ db_conn.php
@@ -1,13 +1,20 @@
 <?php
-$sName = "localhost";
-$uName = "root";
-$pass = "";
-$db_name = "login";
-try {
-   $conn = new PDO("mysql:host=$sName; dbname=$db_name", $uName, $pass);
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-    catch (PDOException $e) {
-     echo "Connection Failed:". $e->getMessage();
-     }
+    class Database {
+        private $host = '127.0.0.1:3306';
+        private $username = 'u977168850_root_capstone';
+        private $password = 'capstone24_rootPass';
+        private $database = 'u977168850_capstone_db';
+        private $connection;
+
+        public function __construct() {
+            $this->connection = new Mysqli($this->host, $this->username, $this->password, $this->database);
+            if ($this->connection->connect_error) {
+                die("Connection failed: " . $this->connection->connect_error);
+            }
+        }
+        
+        public function getConnection() {
+            return $this->connection;
+        }
+    }
 ?>
