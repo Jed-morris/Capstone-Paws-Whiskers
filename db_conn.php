a514bbeb42f2ad144afd569d38c7c08cf37ac16b
Jed-morris <alegre.jed@gmail.com>
2024-03-14 02:33:56 +0800

    Update db connection

--- db_conn.php
+++ db_conn.php
@@ -1,17 +1,28 @@
 <?php
-    class Database {
-        private $host = '127.0.0.1:3306';
-        private $username = 'u977168850_root_capstone';
-        private $password = 'capstone24_rootPass';
-        private $database = 'u977168850_capstone_db';
-        private $connection;
-    }
+class Database {
+    private $host = '127.0.0.1:3306';
+    private $username = 'u977168850_root_capstone';
+    private $password = 'capstone24_rootPass';
+    private $database = 'u977168850_capstone_db';
+    private $connection;
+
+    public function __construct() {
         try {
-         $conn = new PDO("mysql:host=$sName; dbname=$db_name", $uName, $pass);
-         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      }
-          catch (PDOException $e) {
-           echo "Connection Failed:". $e->getMessage();
-           }      
-      ?>
-      
+            $this->connection = new PDO("mysql:host={$this->host}; dbname={$this->database}", $this->username, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Connection Failed: " . $e->getMessage();
+        }
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+}
+
+// Create a new instance of the Database class
+$db = new Database();
+
+// Optionally, you can get the connection object
+$conn = $db->getConnection();
+?>
